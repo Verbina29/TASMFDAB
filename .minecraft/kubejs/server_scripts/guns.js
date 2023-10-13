d90de9df66be195c259e402769590e378096c9a7
Cyrillic2 <semyon.tarasov.2005@mail.ru>
2023-10-14 00:10:47 +0600

    Added KubeJS Create Integration + server script

---
+++ .minecraft/kubejs/server_scripts/guns.js
@@ -0,0 +1,56 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
+
+ServerEvents.recipes(e => {
+  e.recipes.create.mechanical_crafting('alloyedguns:basic_gun_kit', [
+    'PMP',
+    'RCF',
+    'PMP'
+  ], {
+    P: 'thermal:steel_plate',
+	M: 'immersiveengineering:component_iron',
+	R: 'immersiveengineering:stick_steel',
+	C: 'alloyedguns:revolver_cylinder',
+	F: 'flint_and_steel'
+  })
+})
+
+ServerEvents.recipes(e => {
+  e.recipes.create.mechanical_crafting('alloyedguns:advanced_gun_kit', [
+    'BBCBB',
+    'BGMGB',
+	'CFKFC',
+    'BGMGB',
+    'BBCBB'
+  ], {
+    K: 'alloyedguns:basic_gun_kit',
+	M: 'alloyedguns:musket',
+	B: 'alloyedguns:complex_bullet',
+	C: 'immersiveengineering:component_steel',
+	F: 'alloyedguns:flintlock',
+	G: 'thermal:steel_gear'
+  })
+})
+
+ServerEvents.recipes(e => {
+  e.recipes.create.mechanical_crafting('alloyedguns:complex_gun_kit', [
+    'ABBBBBA',
+    'BCDEDCB',
+	'BDDFDDB',
+    'BEFGFEB',
+    'BDDFDDB',
+	'BCDEDCB',
+	'ABBBBBA'
+  ], {
+    A: 'thermal:steel_block',
+	B: 'thermal:steel_plate',
+	C: 'netherite_ingot',
+	D: 'netherite_scrap',
+	E: 'immersiveengineering:gunpowder_barrel',
+	F: 'alloyedguns:basic_gun_kit',
+	G: 'create:precision_mechanism'
+  })
+})
